f13aa781f0025930bd8e972f19581aa64f6cf501
gautamits <gautamamits95@gmail.com>
2016-08-27 20:30:06 +0530

    java 1d and 2d array

---
+++ java/data-structure/java-2d-array.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int i=0;
+        int j=0;
+        int sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+        int sum2;
+        for(i=0; i < 4; i++){
+            for(j=0; j < 4; j++){
+               sum2=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+               if(sum2>sum) sum=sum2;
+            }
+        }
+        System.out.println(sum);
+    }
+}
