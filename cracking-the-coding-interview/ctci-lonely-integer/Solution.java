fecf319e51191ef72407b49504268d28c49b05df
gautamits <gautamamits95@gmail.com>
2017-03-21 23:43:56 +0530

    recursive staircase

---
+++ cracking-the-coding-interview/ctci-lonely-integer/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int first=a[0];
+        for(int i=1;i<n;i++) first=first^a[i];
+        System.out.println(first);
+    }
+}
