255d91a47b17ee7373421b41b0ee531fc1979d68
gautamits <gautamamits95@gmail.com>
2017-12-19 08:38:25 +0530

    the birthday bar

---
+++ algorithms/implementation/the-birthday-bar.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, vector < int > s, int d, int m){
+    // Complete this function
+    int result=0;
+    for(int i=0;i<=n-m;i++){
+        int sum=0;
+        for(int j=i;j<i+m;j++) sum+=s[j];
+        if(sum==d) result+=1;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> s(n);
+    for(int s_i = 0; s_i < n; s_i++){
+       cin >> s[s_i];
+    }
+    int d;
+    int m;
+    cin >> d >> m;
+    int result = solve(n, s, d, m);
+    cout << result << endl;
+    return 0;
+}
