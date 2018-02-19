aecfadf4bbb43e977cc4b02ac3ec2729c144eb21
gautamits <gautamamits95@gmail.com>
2018-02-19 14:27:46 +0530

    happy ladybugs

---
+++ algorithms/implementation/happy-ladybugs.py
@@ -0,0 +1,16 @@
+import re
+
+def calculate(n, b):
+    # no space and same colors are not adjacent
+    if b.count("_") == 0 and len(re.sub(r'((.)\2+)', "", b)) != 0:
+        return "NO"
+    #single letters
+    for a in set(b):
+        if a != "_" and b.count(a) == 1:
+            return "NO"
+    return "YES"
+
+for _ in range(int(input())):
+    n = int(input())
+    b = input()
+    print(calculate(n, b))
